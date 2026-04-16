16f41e935bdc9c9dd9560f722f91cdf68956e633
priyankakapoor12 <priyankakapoor1993@gmail.com>
2026-04-16 23:14:31 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,11 +1,12 @@
-sudo apt install mysql-client -y
-
-mysql -h database-1.cozka4i8kv1n.us-east-1.rds.amazonaws.com -u admin -p
-
 <?php
 $conn = new mysqli("database-1.cozka4i8kv1n.us-east-1.rds.amazonaws.com", "admin", "priyanka", "mydb");
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $name = $_POST['name'];
+
 $conn->query("INSERT INTO users (name) VALUES ('$name')");
 
 echo "User added";
